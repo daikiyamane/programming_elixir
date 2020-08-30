95ddd77b4024f25ab22b70fc8b734397ec6a8eaa
山根大輝 <12183597@g.matsuyama-u.ac.jp>
2020-08-31 00:32:55 +0900

    :update calculate/1 function

--- chapter11/strings_and_binaries.exs
+++ chapter11/strings_and_binaries.exs
@@ -8,4 +8,28 @@ defmodule StringsAndBinaries do
   def anagram?(word1, word2) do
     Enum.sum(word1) === Enum.sum(word2)
   end
+
+  #StringsAndBinaries-4
+  def calculate(formula), do: _digit(formula, 0)
+
+  defp _digit([], value), do: value
+
+  defp _digit([h | t], value) when h in '0123456789' do
+    _digit(t, value * 10 + h - ?0)
+  end
+
+  defp _digit([h | t], value) do
+    case h do
+      43 ->
+        value + _digit(t, 0)
+      45 ->
+        value - _digit(t, 0)
+      42 ->
+        value * _digit(t, 0)
+      47 ->
+        value / _digit(t, 0)
+      _ ->
+        _digit(t, value)
+    end
+  end
 end

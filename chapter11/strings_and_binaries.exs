76e94a5e38ac9cdbca7e3ab9b4f4ac81d9643c7c
山根大輝 <12183597@g.matsuyama-u.ac.jp>
2020-08-31 00:13:08 +0900

    :update anagram?/2 function

--- chapter11/strings_and_binaries.exs
+++ chapter11/strings_and_binaries.exs
@@ -1,5 +1,11 @@
 defmodule StringsAndBinaries do
+  #StringsAndBinaries-1
   def space_to_tilda(list) do
     Enum.all?(list, &(&1 >= 32 and &1 <=126))
+  end
+
+  #StringsAndBinaries-2
+  def anagram?(word1, word2) do
+    Enum.sum(word1) === Enum.sum(word2)
   end
 end
